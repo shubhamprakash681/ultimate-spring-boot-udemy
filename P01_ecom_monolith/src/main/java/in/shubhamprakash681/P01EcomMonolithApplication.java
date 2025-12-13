d9a075ecc5394746e3d8786b8d3f968a18c21675
Shubham Prakash <shubhamprakash681@gmail.com>
2025-12-13 08:14:27 +0530

    FOlder structure changes

--- P01_ecom_monolith/src/main/java/in/shubhamprakash681/P01EcomMonolithApplication.java
+++ P01_ecom_monolith/src/main/java/in/shubhamprakash681/P01EcomMonolithApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class P01EcomMicroserviceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(P01EcomMicroserviceApplication.class, args);
-	}
+public class P01EcomMonolithApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(P01EcomMonolithApplication.class, args);
+    }
 }
--- P01_ecom_monolith/src/test/java/in/shubhamprakash681/P01EcomMicroserviceApplicationTests.java
+++ P01_ecom_monolith/src/test/java/in/shubhamprakash681/P01EcomMicroserviceApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class P01EcomMicroserviceApplicationTests {
+class P01EcomMonolithApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
